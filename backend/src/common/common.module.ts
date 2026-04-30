cc2f3edd309bbc789eadfa4f238f37ea0de5308e
cjacobs <chrisjacobs2468@gmail.com>
2026-04-30 11:50:37 +0100

    Graceful shutdown

--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PiiEncryptionService } from './services/pii-encryption.service';
 import { RateLimitMonitorService } from './services/rate-limit-monitor.service';
+import { GracefulShutdownService } from './services/graceful-shutdown.service';
 
 @Global()
 @Module({
-  providers: [RateLimitMonitorService, PiiEncryptionService],
-  exports: [RateLimitMonitorService, PiiEncryptionService],
+  providers: [RateLimitMonitorService, PiiEncryptionService, GracefulShutdownService],
+  exports: [RateLimitMonitorService, PiiEncryptionService, GracefulShutdownService],
 })
 export class CommonModule {}
